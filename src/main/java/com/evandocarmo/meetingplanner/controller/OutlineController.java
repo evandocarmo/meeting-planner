e9a165daca57c049cb102c5374ed4dd7892ead56
Evan Do Carmo <evan.docarmo@ten24web.com>
2018-01-14 18:41:20 -0500

    added log

--- src/main/java/com/evandocarmo/meetingplanner/controller/OutlineController.java
+++ src/main/java/com/evandocarmo/meetingplanner/controller/OutlineController.java
@@ -5,32 +5,37 @@ import com.evandocarmo.meetingplanner.repository.OutlineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
 
 public class OutlineController {
 
     @Autowired
     OutlineRepository outlineRepository;
+    
+    @GetMapping("/outlines")
+    public String outlineList(Model model){
+    		return "outlinelist";
+    }
 
     // Get All Outlines
-    @GetMapping("/outlines")
+    @GetMapping("/api/outlines")
     public List<Outline> getAllOutlines() {
         return outlineRepository.findAll();
     }
 
     // Create a new outlines
-    @PostMapping("/outlines")
+    @PostMapping("/api/outlines")
     public Outline createOutline(@Valid @RequestBody Outline outline) {
         return outlineRepository.save(outline);
     }
 
     // Get a Single Outline
-    @GetMapping("/notes/{id}")
+    @GetMapping("/api/outlines/{id}")
     public ResponseEntity<Outline> getOutlineById(@PathVariable(value = "id") Long outlineId) {
         Outline outline = outlineRepository.findOne(outlineId);
         if(outline == null) {
@@ -39,7 +44,7 @@ public class OutlineController {
         return ResponseEntity.ok().body(outline);
     }
 
-    // Update a Note
+    // Update
 
-    // Delete a Note
+    // Delete
 }
